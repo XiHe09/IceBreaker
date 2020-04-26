1959fb0348104ee0f24d50bcf8e2f72a40333246
Emily Zhai <emilyzhai@emilydembp.win.ad.jhu.edu>
2020-04-25 22:10:58 -0400

    set up drawer for IF section

---
+++ app/src/main/java/com/example/icebreaker/IfActivity.java
@@ -0,0 +1,61 @@
+package com.example.icebreaker;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class IfActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_if);
+        Toolbar toolbar = findViewById(R.id.WToolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        // Handle navigation view item clicks here.
+        int id = menuItem.getItemId();
+        if (id == R.id.drawer_home) {
+            Intent intent = new Intent(IfActivity.this, MainActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.drawer_or) {
+            Intent intent = new Intent(IfActivity.this, OrActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.drawer_w) {
+            Intent intent = new Intent(IfActivity.this, WActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.drawer_game) {
+            Intent intent = new Intent(IfActivity.this, GameActivity.class);
+            startActivity(intent);
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
